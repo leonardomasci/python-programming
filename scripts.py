8ec04af4db6ee5e24691bf0ffc0ddc00066944a0
leonardomasci <71600287+leonardomasci@users.noreply.github.com>
2020-10-22 16:36:58 +0200

    Add files via upload

---
+++ scripts.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Introduction
+
+
+# In[2]:
+
+
+#Say "Hello, World!" With Python
+print("Hello, World!")
+
+
+# In[ ]:
+
+
+#Python If-Else
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+if n//2 != n/2 :
+    print ('Weird')
+elif n<=5 :
+    print ('Not Weird')
+elif n<=20 :
+    print ('Weird')
+else:
+    print ('Not Weird')
+
+
+# In[ ]:
+
+
+#Arithmetic Operators
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print (a+b)
+    print (a-b)
+    print (a*b)
+
+
+# In[ ]:
+
+
+
+
